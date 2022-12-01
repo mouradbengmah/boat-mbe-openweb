ac7e93eab24a5c47c54d38bdc909173d76f07f8b
Mourad Bengmah <mourad.bengmah@gmail.com>
2022-12-01 21:18:56 +0100

    Ajout du modèle des bateaux.

---
+++ boat-back/src/main/java/com/boat/model/Boat.java
@@ -0,0 +1,28 @@
+package com.boat.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "boat")
+public class Boat {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+	private long id;
+	
+	@Column(name = "nom")
+	private String nom;
+	
+	@Column(name = "description")
+	private String description;
+}
