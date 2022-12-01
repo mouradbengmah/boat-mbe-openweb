df716e9f76bd14ddc5259157caa98529c441087c
Mourad Bengmah <mourad.bengmah@gmail.com>
2022-12-01 20:47:35 +0100

    Initialisation du back.

---
+++ boat-back/src/main/java/com/boat/BoatBackApplication.java
@@ -0,0 +1,13 @@
+package com.boat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatBackApplication.class, args);
+	}
+
+}
---
+++ boat-back/src/test/java/com/boat/BoatBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
