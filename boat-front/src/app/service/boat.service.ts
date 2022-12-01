bfb7c8cd4471f44dd5c314e2ecc080e63cd56fd7
Mourad Bengmah <mourad.bengmah@gmail.com>
2022-12-01 22:43:30 +0100

    Création du service des boats.

---
+++ boat-front/src/app/service/boat.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Boat } from '../model/Boat';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoatService {
+
+  private boatApiUrl = environment.boatApiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  recupererLesBateaux(): Observable<Boat[]> {
+    return this.http.get<Boat[]>(this.boatApiUrl + '/boat/');
+  }
+
+  recupererUnBateauParId(id: String): Observable<Boat> {
+    return this.http.get<Boat>(this.boatApiUrl + '/boat/' + id);
+  }
+
+  enregistrerUnBateau(nom: string, description: string): Observable<string> {
+    let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    let options = { headers: headers };
+    let jsonObject = this.preparerJsonObject(nom, description);
+    return this.http.post<string>(
+      this.boatApiUrl + '/boat/',
+      jsonObject,
+      options
+    );
+  }
+
+  private preparerJsonObject(nom: string, description: string) {
+    const object = {
+      nom: nom,
+      description: description
+    }
+    return JSON.stringify(object);
+  }
+}
