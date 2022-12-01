8355c3c939495f761bafbcfbe0cf186ac3ee3456
Mourad Bengmah <mourad.bengmah@gmail.com>
2022-12-01 23:13:51 +0100

    Ajout des modules Material et Forms.

--- boat-front/src/app/app.module.ts
+++ boat-front/src/app/app.module.ts
@@ -5,16 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BoatComponent } from './boat/boat.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatListModule } from '@angular/material/list';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoatComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatListModule,
+    DragDropModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
